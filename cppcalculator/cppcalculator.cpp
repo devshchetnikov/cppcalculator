e137e5adf7f321dc113cf5258bf745846a2deb4d
devshchetnikov <dmitrijsetnikov87@gmail.com>
2026-08-22 14:20:59 +0500

    Добавьте файлы проекта.

---
+++ cppcalculator/cppcalculator.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "RU");
+
+	double num1, num2;
+	char op;
+
+	std::cout << "Введите первое число: ";
+	std::cin >> num1;
+
+	std::cout << "Выберите операцию (+, -, *, /): ";
+	std::cin >> op;
+
+	std::cout << "Введите второе число: ";
+	std::cin >> num2;
+
+	switch (op) {
+	case '+':
+		std::cout << num1 + num2 << std::endl;
+		break;
+	case '-':
+		std::cout << num1 - num2 << std::endl;
+		break;
+	case '*':
+		std::cout << num1 * num2 << std::endl;
+		break;
+	case '/':
+		if (num2 == 0) {
+			std::cout << "Ошибка: Деление на ноль невозможно!" << std::endl;
+		}
+		else {
+			std::cout << "Результат: " << num1 / num2 << std::endl;
+		}
+		break;
+	}
+
+	return 0;
+}
